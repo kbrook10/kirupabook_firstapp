326db027a28dee0a2f2045ca843adb9e31b94f1c
keith brooks <kbrook10@gmail.com>
2016-12-04 17:31:43 -0500

    Update initial files for chapter 4 styling lesson

---
+++ js/styling.js
@@ -0,0 +1,22 @@
+var destination = document.getElementById('container')
+
+class Letter extends React.Component {
+    render() {
+        return(
+            <div>
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <div>
+        <Letter>A</Letter>
+        <Letter>E</Letter>
+        <Letter>I</Letter>
+        <Letter>O</Letter>
+        <Letter>U</Letter>
+    </div>,
+    destination
+)
