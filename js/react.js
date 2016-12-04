4675d67063bae9167058c5251e79da16842e4ae6
keith brooks <kbrook10@gmail.com>
2016-12-04 15:13:52 -0500

    Identify why components are great by reducing duplicated code

--- js/react.js
+++ js/react.js
@@ -1,6 +1,10 @@
 var destination = document.getElementById('container');
 
 ReactDOM.render(
-    <h1>Keith Brooks</h1>,
+    <div>
+        <h3><i>Batman</i></h3>
+        <h3><i>Iron Man</i></h3>
+        <h3><i>Mega Man</i></h3>
+    </div>,
     destination
 )
