ee5a725ca2afb6d640e4f3c1f397fec67f7cf914
keith brooks <kbrook10@gmail.com>
2016-12-05 21:24:17 -0500

    Update react router properties and add IndexLink to prevent default root view from always having highlighted link

--- js/spa.js
+++ js/spa.js
@@ -1,8 +1,72 @@
 var destination = document.getElementById('container')
 
+var { Router, Route, IndexRoute, IndexLink, Link} = ReactRouter;
+
+class Home extends React.Component {
+    render() {
+        return(
+            <div>
+                <h2>HELLO</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+        )
+    }
+}
+
+class Contact extends React.Component {
+    render() {
+        return(
+            <div>
+                <h2>GOT QUESTIONS</h2>
+                <p>The easist thing to do is post on our <a href='http://forum.kirupa.com'>forum</a></p>
+            </div>
+        )
+    }
+}
+class Stuff extends React.Component {
+    render() {
+        return(
+            <div>
+                <h2>Stuff</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <ol>
+                    <li>Nulla pulvinar diam</li>
+                    <li>et dolore magna aliqua</li>
+                    <li> magna aliqua</li>
+                    <li>dolore magna</li>
+                    <li>ulvinar diam</li>
+                </ol>
+            </div>
+        )
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Simple SPA</h1>
+                <ul className='header'>
+                    <li><IndexLink to='/' activeClassName='active'>Home</IndexLink></li>
+                    <li><Link to='/stuff' activeClassName='active'>Stuff</Link></li>
+                    <li><Link to='/contact' activeClassName='active'>Contact</Link></li>
+                </ul>
+                <div className='content'>
+                    {this.props.children}
+                </div>
+            </div>
+        )
+    }
+}
+
 ReactDOM.render(
-    <div>
-        <h1>Hello World!</h1>
-    </div>,
+    <Router>
+        <Route path='/' component={App}>
+            <IndexRoute component={Home}/>
+            <Route path='stuff' component={Stuff}/>
+            <Route path='contact' component={Contact}/>
+        </Route>
+    </Router>,
     destination
 )
