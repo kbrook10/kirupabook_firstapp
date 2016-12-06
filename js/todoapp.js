f19bef10aec0ebe5947f6dfbfb792cf8a04cf248
keith brooks <kbrook10@gmail.com>
2016-12-06 05:22:00 -0500

    Update to add method to add new tasks to the array and prevent default onSubmit functionality of the form to POST and refresh the page

--- js/todoapp.js
+++ js/todoapp.js
@@ -4,16 +4,29 @@ var { Router, Route, IndexRoute, IndexLink, Link} = ReactRouter;
 class TodoList extends React.Component {
     constructor(props) {
         super(props)
+        this.addItem = this.addItem.bind(this)
         this.state = {
             items: []
         }
+    }
+    addItem(event) {
+        var itemArray = this.state.items;
+
+        itemArray.push({
+            text: this._inputElement.value,
+            key: Date.now()
+        });
+        this.setState({
+            items: itemArray
+        });
+        event.preventDefault();
     }
     render() {
         return(
             <div className='todoListMain'>
                 <div className='header'>
-                    <form>
-                        <input placeholder='enter task'>
+                    <form onSubmit={this.addItem}>
+                        <input ref={(a) => this._inputElement = a} placeholder='enter task'>
                         </input>
                         <button type='submit'>add</button>
                     </form>
